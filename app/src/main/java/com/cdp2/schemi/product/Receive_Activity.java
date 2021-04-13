d91f369ca7ec045817f1c317b25ded6439085ba8
young315 <young315@knu.ac.kr>
2021-04-13 19:59:47 +0900

    receive layout & receive activity

--- app/src/main/java/com/cdp2/schemi/product/Receive_Activity.java
+++ app/src/main/java/com/cdp2/schemi/product/Receive_Activity.java
@@ -1,4 +1,30 @@
 package com.cdp2.schemi.product;
 
-public class Receive_Activity {
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.cdp2.schemi.R;
+
+public class Receive_Activity extends AppCompatActivity {
+    String TAG = "Receive_Activity";
+
+    TextView mTv_curr_warehouse;
+    TextView mTv_warehouse_name;
+    TextView mTv_qr_photo;
+    TextView mTv_label_photo;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.receive_layout);
+
+        mTv_curr_warehouse=findViewById(R.id.receive_tv_curr_warehouse);
+        mTv_warehouse_name=findViewById(R.id.receive_tv_warehouse_name);
+        mTv_qr_photo=findViewById(R.id.receive_tv_qr_photo);
+        mTv_label_photo=findViewById(R.id.receive_tv_label_photo);
+    }
 }
